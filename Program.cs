ef6acda3e872171e4e68295189d7525459120d37
paullipnica <163457799+paullipnica@users.noreply.github.com>
2024-06-19 12:05:41 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Connect4Game
+{
+    class GameGrid
+    {
+       
+
+        public ConnectGrid()
+        {
+          
+        }
+
+        public void DropPiece()
+        {
+            
+           
+           
+        }
+
+        private bool CheckWin()
+        {
+           
+        }
+
+        private bool CheckLine()
+        {
+           
+        }
+
+        private bool CheckDirection()
+        {
+           
+        }
+
+        public void DisplayGrid()
+        {
+           
+        }
+
+        public bool IsColumnFull()
+        {
+            
+        }
+    }
+
+    class GameMaster
+    {
+        
+        public void StartGame()
+        {
+         
+      
+        }
+
+        private void PlayGame()
+        {
+            
+        }
+
+        private void PlayerTurn(bool isPlayer1)
+        {
+           
+        }
+
+        private void AITurn()
+        {
+            
+        }
+
+        public void Winner()
+        {
+          
+        }
+    }
+
+    abstract class Player
+    {
+        public abstract void TakeTurn();
+    }
+
+    class HumanPlayer : Player
+    {
+        public override void TakeTurn()
+        {
+            
+        }
+    }
+
+    class AIPlayer : Player
+    {
+       
+    }
+
+    class Program
+    {
+
+    }
+}
